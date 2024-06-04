7ed77b90155e6f85ba58cdf00226f6aca4b0d148
chrisvasqm <christianvasquezmelo@gmail.com>
2024-06-04 18:20:49 -0400

    Improve security

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import 'dotenv/config';
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.use(helmet());
+
   const port = process.env.PORT || 3000;
   await app.listen(port, () =>
     console.log(`Server listening on http://localhost:${port}`),
