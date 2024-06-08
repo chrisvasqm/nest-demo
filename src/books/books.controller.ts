c29621ccd172d5068ca00702ffbf56a2eae22124
chrisvasqm <christianvasquezmelo@gmail.com>
2024-06-08 17:36:11 -0400

    Add Not Found samples

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -11,8 +11,9 @@ import {
   Put,
   Query
 } from '@nestjs/common';
-import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateBookDto, UpdateBookDto } from './books.dto';
+import { Book } from './books.entity';
 import { BooksService } from './books.service';
 
 @ApiTags('Books')
@@ -23,12 +24,15 @@ export class BooksController {
   @Get()
   @ApiOperation({ summary: 'Get all Books' })
   @ApiQuery({ name: 'genre', required: false, description: 'Used to filter Books by their genre' })
+  @ApiResponse({ status: 200, type: Book, isArray: true })
   getAll(@Query('genre') genre?: string) {
     return this.service.getAll({ genre });
   }
 
   @Get(':id')
   @ApiOperation({ summary: 'Find a given Book by their id' })
+  @ApiResponse({ status: 200, type: Book })
+  @ApiResponse({ status: 404, description: 'Not found' })
   find(@Param('id', ParseIntPipe) id: number) {
     return this.service.find(id);
   }
@@ -36,18 +40,23 @@ export class BooksController {
   @Post()
   @HttpCode(HttpStatus.CREATED)
   @ApiOperation({ summary: 'Creates a new Book' })
+  @ApiResponse({ status: 201, type: Book })
   create(@Body() book: CreateBookDto) {
     return this.service.create(book);
   }
 
   @Put(':id')
   @ApiOperation({ summary: 'Updates a Book by their id' })
+  @ApiResponse({ status: 200, type: Book })
+  @ApiResponse({ status: 404, description: 'Not found' })
   update(@Param('id', ParseIntPipe) id: number, @Body() book: UpdateBookDto) {
     return this.service.update(id, book);
   }
 
   @Delete(':id')
   @ApiOperation({ summary: 'Deletes a Book by their id' })
+  @ApiResponse({ status: 200, type: Book })
+  @ApiResponse({ status: 404, description: 'Not found' })
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.service.delete(id);
   }
