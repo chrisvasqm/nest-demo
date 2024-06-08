3c0a005bc80097fa8b52660540e4ef297d096046
chrisvasqm <christianvasquezmelo@gmail.com>
2024-06-08 17:36:03 -0400

    Add Swagger schema definition

--- src/books/books.entity.ts
+++ src/books/books.entity.ts
@@ -1,13 +1,17 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Book {
   @PrimaryGeneratedColumn()
+  @ApiProperty()
   id: number;
 
   @Column()
+  @ApiProperty()
   name: string;
 
   @Column()
+  @ApiProperty()
   genre: string;
 }
