02d0e14fd7bec88bc99121347cdd535d4597c26e
chrisvasqm <christianvasquezmelo@gmail.com>
2024-06-15 18:22:05 -0400

    Improve DTO validation

--- src/books/books.dto.ts
+++ src/books/books.dto.ts
@@ -1,25 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, MinLength } from 'class-validator';
+import { IsNotEmpty, isString, IsString, MinLength } from 'class-validator';
 
 export class CreateBookDto {
-  @MinLength(1)
+  @IsString()
   @IsNotEmpty()
   @ApiProperty()
   name: string;
 
-  @MinLength(1)
+  @IsString()
   @IsNotEmpty()
   @ApiProperty()
   genre: string;
 }
 
 export class UpdateBookDto {
-  @MinLength(1)
+  @IsString()
   @IsNotEmpty()
   @ApiProperty()
   name: string;
 
-  @MinLength(1)
+  @IsString()
   @IsNotEmpty()
   @ApiProperty()
   genre: string;
